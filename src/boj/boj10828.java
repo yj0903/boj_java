b3b5a29da2b12474bcf24704680e545ae74d34f1
yj0903 <wiseidea1@hanmail.net>
2022-07-20 11:20:40 +0900

    boj10828 스택 시간초과 해결 (BufferedReader,BufferedWriter 사용)

--- src/boj/boj10828.java
+++ src/boj/boj10828.java
@@ -1,22 +1,28 @@
 // boj10828 - 스택
 package boj;
 
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
 
 public class boj10828 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        Stack stack = new Stack();
-        sc.nextLine(); // Delete enter value
+    public static void main(String[] args) throws IOException {
+        // 시간초과 해결하기) 스캐너 대신 버퍼리더, 버퍼라이터 사용
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 버퍼리더
+        int num = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < num; i++) {
-            String[] arr = sc.nextLine().split(" ");
+            String[] arr = br.readLine().split(" ");
             switch (arr[0]) {
                 case "push":
-                    stack.add(arr[1]);
+                    stack.push(Integer.parseInt(arr[1]));
                     break;
                 case "top":
                     if (!stack.empty()) {
